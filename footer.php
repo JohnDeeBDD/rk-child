eeb0994a57ed0e770dcd5ae6ce84560fd97e072a
Ubuntu <ubuntu@ip-172-31-38-41.ec2.internal>
2019-12-28 03:30:57 +0000

    asdf

---
+++ footer.php
@@ -0,0 +1,12 @@
+<?php
+?>
+	</div><!-- #content -->
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			&copy; <?php echo esc_attr( date_i18n( __( 'Y', 'responsive-kubrick' ) ) ); ?> <?php esc_html( bloginfo( 'name' ) ); ?>	
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+<?php wp_footer(); ?>
+</body>
+</html>
